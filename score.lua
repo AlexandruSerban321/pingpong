75c50ef9ccbe73c73a613204003ccd0283670ee0
AlexandruSerban321 <alexandruserban160@gmail.com>
2021-02-21 23:31:09 +0200

    Fixing typos

--- score.lua
+++ score.lua
@@ -24,7 +24,7 @@ function Score:winCondition()
     Ball.yVel = 0
     Ball.x = love.graphics.getWidth()/2 - Ball.width/2
     Ball.y = love.graphics.getHeight()/2 - Ball.height/2
-    Player.y = love.graphics.getHeight/2 - Player.height/2
+    Player.y = love.graphics.getHeight()/2 - Player.height/2
     Ai.y = Player.y
     self.playerScore = self.playerScore+1
   end
